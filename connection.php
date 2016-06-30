7918ffe8a8feb7a8444b30d7de2063c4c5d75f7a
odorsey <oddorsey@gmail.com>
2016-06-30 14:35:31 -0400

    DB info for hosting

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
   $h = 'localhost';
-  $u = 'root';
-  $p = '';
-  $dbname = 'portal';
+  $u = 'visuame5_patient';
+  $p = '@cc3ssGiven!';
+  $dbname = 'visuame5_portal';
   $db = mysqli_connect($h, $u, $p, $dbname);
   if (mysqli_connect_errno()) {
     echo "Cannot connect: " . mysqli_connect_error();
