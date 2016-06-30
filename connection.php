59f0bd4c618c61ed52fe666607ab0009f6d8fc3a
odorsey <oddorsey@gmail.com>
2016-06-30 15:08:31 -0400

    Fixed password

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
   $h = 'localhost';
-  $u = 'visuame5_patient';
-  $p = '@cc3ssGiven!';
-  $dbname = 'visuame5_portal';
+  $u = '';
+  $p = '';
+  $dbname = 'portal';
   $db = mysqli_connect($h, $u, $p, $dbname);
   if (mysqli_connect_errno()) {
     echo "Cannot connect: " . mysqli_connect_error();
